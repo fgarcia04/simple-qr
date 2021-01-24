ebbc5719b83a93095d8c4b9fb9f8d49d3a3f1b2a
fgarcia <fgarcia@urbano.com.ar>
2021-01-23 23:19:49 -0300

    rollback modules translate and barcode

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,20 +9,15 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { TranslateModule } from '@ngx-translate/core';
-import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
-
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [
     BrowserModule,
      IonicModule.forRoot(),
-      AppRoutingModule,
-      TranslateModule
+      AppRoutingModule
     ],
   providers: [
-    BarcodeScanner,
     StatusBar,
     SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
