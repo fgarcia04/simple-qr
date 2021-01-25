834ac3b332d3436b2063177ce9cec5879d47b68c
fgarcia <fgarcia@urbano.com.ar>
2021-01-25 13:58:51 -0300

    Model registry

---
+++ src/app/model/registry.model.ts
@@ -0,0 +1,36 @@
+export class Registry{
+
+    private format: string;
+    private text: string;
+    private type: string;
+    private icon: string;
+    private created: Date;
+
+    constructor( format: string, text: string ){
+
+        this.format = format;
+        this.format = format;
+        this.created =  new Date();
+        this.getType();
+
+    }
+
+    private getType(){
+        const type = this.text.substr(0, 4);
+        switch(type){
+            case 'http':
+                this.type = 'http';
+                this.icon = 'globe';
+                break;
+            case 'geo:':
+                this.type = 'geo';
+                this.icon = 'pin';
+                break;
+            default:
+                this.type = 'Non';
+                this.icon = 'create'
+
+        }
+    }
+    
+}
